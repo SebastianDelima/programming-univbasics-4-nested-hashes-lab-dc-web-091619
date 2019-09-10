99c372551701923ac41d95ce326032804d6dec14
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-10 19:32:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -142,6 +142,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
- return programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+ return programmer_hash[:dennis_ritchie][:languages] = "Assembly"
  
 end
